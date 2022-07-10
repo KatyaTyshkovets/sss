c97b19b376f19a08605afac2b22b6c287acc3cce
KatyaTyshkovets <nesterchuck91@gmail.com>
2022-07-10 09:16:03 +0300

    mmm

--- src/index.js
+++ src/index.js
@@ -7,24 +7,24 @@
         prevEl: '.swiper-button-prev'
     },
     // булети,пагинація
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-        renderFraction: function (currentClass, totalClass) {
-            return 'Фото <span class="' + currentClass + '"></span>' +
-                ' из '
-                + '<span class="' + totalClass + '"></span>';
-        }
-    },
-    // scroll
+     pagination: {
+         el: '.swiper-pagination',
+          type: 'bullets',
+        //   renderFraction: function (currentClass, totalClass) {
+        //       return 'Фото <span class="' + currentClass + '"></span>' +
+        //           ' из '
+        //           + '<span class="' + totalClass + '"></span>';
+        //   }
+     },
+    //  scroll
 
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true
-    },
+     scrollbar: {
+         el: '.swiper-scrollbar',
+         draggable: true
+     },
 
     simulateTouch: true,
-    touchRatio: 2,
+    touchRatio: 1,
     touchAngle: 45,
     grabCursor: true,
     slideToClickedSlide: true,
@@ -45,41 +45,52 @@
     autoHeight: true,
 
 
-    // slidesPerView: 3,
+     slidesPerView: 1,
 
-    // slidesPerGroup: 2,
+     slidesPerGroup: 1,
 
     watchOverflow: true,
 
-    spaceBetween: 30,
+    spaceBetween: 10,
 
-    loop: false,
+    loop: true,
 
      autoplay: {
          delay: 2000,
          stopOnLastSlide: true,
          disableOnInteraction: false,
      },
-    speed: 800,
+    speed: 1000,
     
-    
+    // effect:'slide',
+
+    // effect: 'fade',
 
 
-    //  effect: 'flip',
-    //  flipEffect: {
-    //      slideShadows: true,
-    //      limitRotation: true
-    //  },
 
-     effect: 'cube',
+    //   effect: 'flip',
+    //   flipEffect: {
+    //       slideShadows: true,
+    //       limitRotation: true
+    //   },
 
-     cubeEffect: {
+     // effect: 'cube',
+
+    //   cubeEffect: {
+    //       slideShadows: true,
+    //       shadow: true,
+    //       shadowOffset: 20,
+    //       shadowScale: 0.94
+    //   },
+
+
+     effect: 'coverflow',
+     coverFlowEffect: {
+         rotate: 30,
+         stretch: 50,
          slideShadows: true,
-         shadow: true,
-         shadowOffset: 20,
-         shadowScale: 0.94
      },
-
+    
     //  breakpoints: {
     //          '@.075': {
     //             slidesPerView: 1,
@@ -94,18 +105,18 @@
     //              slidesPerView: 1,
     //          },
     //     },
-//      breakpoints: {
-//          320: {
-//              slidesPerView: 1,
-//          },
-//          768: {
-//              slidesPerView: 1,
-//          },
-//          992: {
-//      slidesPerView: 1,
-//  },
+      breakpoints: {
+          320: {
+              slidesPerView: 1,
+          },
+          768: {
+              slidesPerView: 2,
+          },
+          992: {
+      slidesPerView: 3,
+  },
 
-//      },
+      },
     
 
     preloadImages: false,
@@ -122,16 +133,42 @@
     observeParents: true,
     observeSlideChildren: true,
 
-    // swiper.on: {
-    //     init: function() {
-    //         console.log("слайдер запущен");
-    //     },
-    //     slideChange: function () {
-    //         console.log("слайд переключен");
-    //     }
-    // },
+    
 
 });
+
+ const onCloseSlider = document.querySelector('.slider-on');
+const onSliderOpen = document.querySelector('.slider-open');
+const wrapper = document.querySelector('.wrapper');
+
+onSliderOpen.addEventListener('click', toogleSliderHandler);
+wrapper.addEventListener('click', onBackdropClick);
+
+
+function toogleSliderHandler() {
+ window.addEventListener('keydown', onEscKeyPress);
+    wrapper.classList.toggle('hide'); 
+} 
+ 
+ function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+        toogleSliderHandler();
+        console.log('click');
+    }
+  }
+
+   function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+
+    if (isEscKey) {
+      toogleSliderHandler();
+    }
+  }
+
+
+
+
 
 
 //  let sliderBlock = document.querySelector('.image-slider');
@@ -169,39 +206,18 @@
 
 
 
-// const sliderClick = document.querySelector('.footer-modal-btn');
+
 
 
  
 
-//  sliderClick.addEventListener('click', handleClickModalBtn);
-//  refs.closeModalBtn.addEventListener('click',handleCloseSlider );
-//  refs.backdrop.addEventListener('click', onBackdropClick);
 
-//  export function handleClickModalBtn() {
-//    window.addEventListener('keydown', onEscKeyPress);
-//    document.body.classList.add('show-slider');
-//  }
 
-//  export function handleCloseSlider() {
-//    window.removeEventListener('keydown', onEscKeyPress);
-//    document.body.classList.remove('show-slider');
-//  }
 
-//  export function onBackdropClick(event) {
-//    if (event.currentTarget === event.target) {
-//      handleCloseSlider();
-//    }
-//  }
 
-//  export function onEscKeyPress(event) {
-//    const ESC_KEY_CODE = 'Escape';
-//    const isEscKey = event.code === ESC_KEY_CODE;
 
-//    if (isEscKey) {
-//      handleCloseSlider();
-//    }
-//  }
+
+
 
 
 
